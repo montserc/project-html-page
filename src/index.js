1f6a304e39ccc51e5b99f317e05aa8e6942db123
montserc <mrcatasus@gmail.com>
2021-03-14 19:54:56 +0100

    html: internal links and sections. js: listeners to display sections

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log('Hola');
+let displays = [
+    { when: "a-key", hide: ["done-by","uso-adecuado-epis","higiene-personal","instalaciones"], show: ["key-project"] },
+    { when: "a-epis", hide: ["key-project","higiene-personal","instalaciones"], show: ["done-by","uso-adecuado-epis"] },
+    { when: "a-personal", hide: ["key-project","uso-adecuado-epis","instalaciones"], show: ["done-by", "higiene-personal"] },
+    { when: "a-instalaciones", hide: ["key-project","uso-adecuado-epis","higiene-personal"], show: ["done-by", "instalaciones"] },
+    { when: "a-enviar", hide: ["key-project"], show: ["done-by"] },
+]
+
+document.addEventListener('DOMContentLoaded',function() {
+    render();
+});
+
+function render() {
+    displays.map(displayItem);
+}
+
+function displayItem(dp, icurrent, dps) {    
+    let a = document.getElementById(dp.when);
+    a.addEventListener('click',function(event) {
+        event.preventDefault();
+        console.log(a.text);
+        dp.hide.map(i => document.getElementById(i).style.display = 'none');
+        dp.show.map(i => document.getElementById(i).style.display = 'block');
+        
+        dps.filter((item, index) => index != icurrent).forEach(element => { 
+            document.getElementById(element.when).parentElement.style.background='white';
+            document.getElementById(element.when).parentElement.style.fontWeight='normal';
+        });        
+
+        dps.filter((item, index) => index == icurrent).forEach(element => {
+            document.getElementById(element.when).parentElement.style.background='#FFFFE0';
+            document.getElementById(element.when).parentElement.style.fontWeight='bold';
+        });        
+
+    });
+}
+
+
