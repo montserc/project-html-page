3ddb5262a7a0c770528ba1b127660c6e64f1a8d0
montserc <mrcatasus@gmail.com>
2021-03-19 09:28:43 +0100

    Estructura definida y funcional

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 let displays = [
-    { when: "a-key", hide: ["done-by","uso-adecuado-epis","higiene-personal","instalaciones"], show: ["key-project"] },
-    { when: "a-epis", hide: ["key-project","higiene-personal","instalaciones"], show: ["done-by","uso-adecuado-epis"] },
-    { when: "a-personal", hide: ["key-project","uso-adecuado-epis","instalaciones"], show: ["done-by", "higiene-personal"] },
-    { when: "a-instalaciones", hide: ["key-project","uso-adecuado-epis","higiene-personal"], show: ["done-by", "instalaciones"] },
-    { when: "a-enviar", hide: ["key-project"], show: ["done-by"] },
+    { when: "a-key", hide: ["done-by","uso-adecuado-epis","higiene-personal","instalaciones"], show: ["key-project"], title: "a big HTML page" },
+//    { when: "a-page", hide: ["empty"], show: ["key-project","done-by","uso-adecuado-epis","higiene-personal","instalaciones"], title: "Auditoria Covid-19" },
+    { when: "a-epis", hide: ["key-project","higiene-personal","instalaciones"], show: ["done-by","uso-adecuado-epis"], title: "Auditoria Covid-19" },
+    { when: "a-personal", hide: ["key-project","uso-adecuado-epis","instalaciones"], show: ["done-by", "higiene-personal"], title: "Auditoria Covid-19" },
+    { when: "a-instalaciones", hide: ["key-project","uso-adecuado-epis","higiene-personal"], show: ["done-by", "instalaciones"], title: "Auditoria Covid-19" },
+    { when: "a-enviar", hide: ["key-project"], show: ["done-by"], title: "Auditoria Covid-19" },
 ]
 
 document.addEventListener('DOMContentLoaded',function() {
@@ -23,16 +24,17 @@ function displayItem(dp, icurrent, dps) {
         dp.show.map(i => document.getElementById(i).style.display = 'block');
         
         dps.filter((item, index) => index != icurrent).forEach(element => { 
-            document.getElementById(element.when).parentElement.style.background='white';
-            document.getElementById(element.when).parentElement.style.fontWeight='normal';
+            document.getElementById(element.when).parentElement.classList.add('menu-item-unclicked');
+            document.getElementById(element.when).parentElement.classList.remove('menu-item-clicked');
+
         });        
 
         dps.filter((item, index) => index == icurrent).forEach(element => {
-            document.getElementById(element.when).parentElement.style.background='#FFFFE0';
-            document.getElementById(element.when).parentElement.style.fontWeight='bold';
+            document.getElementById(element.when).parentElement.classList.remove('menu-item-unclicked');
+            document.getElementById(element.when).parentElement.classList.add('menu-item-clicked');
         });        
-
+        
+        document.getElementById('title').textContent = dp.title;
+        
     });
 }
-
-
