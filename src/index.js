bf6a4c4390b6e9b0d6264989f38c767f5c4a2c5a
montserc <mrcatasus@gmail.com>
2021-03-21 15:37:09 +0100

    Practicamente acabado. Falta la funcionalidad del link Enviar.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,52 @@
 let displays = [
-    { when: "a-key", hide: ["done-by","uso-epis","higiene-personal","instalaciones","enviar"], show: ["key-project"], title: "a big HTML page" },
-    { when: "a-epis", hide: ["key-project","higiene-personal","instalaciones","enviar"], show: ["done-by","uso-epis"], title: "Auditoria Covid-19" },
-    { when: "a-personal", hide: ["key-project","uso-epis","instalaciones","enviar"], show: ["done-by", "higiene-personal"], title: "Auditoria Covid-19" },
-    { when: "a-instalaciones", hide: ["key-project","uso-epis","higiene-personal","enviar"], show: ["done-by", "instalaciones"], title: "Auditoria Covid-19" },
-    { when: "a-enviar", hide: ["key-project","uso-epis","higiene-personal","instalaciones"], show: ["done-by","enviar"], title: "Auditoria Covid-19" },
+    { 
+        when: "a-key", 
+        hide: ["done-by","uso-epis","higiene-personal","instalaciones","enviar","a-epis","a-personal","a-instalaciones","a-enviar"], 
+        show: ["key-project","a-bigpage","empty"], 
+        title: "A big HTML Page" 
+    },
+    { 
+        when: "a-bigpage", 
+        hide: ["key-project","higiene-personal","instalaciones","enviar","a-bigpage","empty"], 
+        show: ["done-by","uso-epis","a-epis","a-personal","a-instalaciones","a-enviar"], 
+        title: "Auditoria Covid-19" 
+    },
+    { 
+        when: "a-epis", 
+        hide: ["key-project","higiene-personal","instalaciones","enviar","a-bigpage","empty"], 
+        show: ["done-by","uso-epis"], 
+        title: "Auditoria Covid-19" 
+    },
+    { 
+        when: "a-personal", 
+        hide: ["key-project","uso-epis","instalaciones","enviar","a-bigpage","empty"], 
+        show: ["done-by", "higiene-personal"], 
+        title: "Auditoria Covid-19" 
+    },
+    { 
+        when: "a-instalaciones", 
+        hide: ["key-project","uso-epis","higiene-personal","enviar","a-bigpage","empty"], 
+        show: ["done-by", "instalaciones"], 
+        title: "Auditoria Covid-19" 
+    },
+    { 
+        when: "a-enviar", 
+        hide: ["key-project","uso-epis","higiene-personal","instalaciones"], 
+        show: ["done-by","enviar"], 
+        title: "Auditoria Covid-19" 
+    }
 ]
 
 document.addEventListener('DOMContentLoaded',function() {
     render();
-    document.getElementById('a-key').click();    
+    document.getElementById('a-key').click();
 });
 
+let radios;
 function render() {
-    /*
-    document.getElementById('img-view').style.display='none';
-    document.getElementById('a-imagen').addEventListener('click',function(event) {
-        event.preventDefault();
-        document.getElementById('img-view').style.display='block';
-    });
-    */
-    displays.map(displayItem);
-    addRadioListeners();
+    radios = Object.values(document.getElementsByTagName('input')).filter((item) => item.type == "radio");
+    radios.map(addRadioListener);
+    displays.map(displayItem);    
 }
 
 function displayItem(dp, icurrent, dps) {    
@@ -39,31 +65,39 @@ function displayItem(dp, icurrent, dps) {
         dps.filter((item, index) => index == icurrent).forEach(element => {
             document.getElementById(element.when).classList.remove('unclicked');
             document.getElementById(element.when).classList.add('clicked');
-        });        
+        }); 
         
-        document.getElementById('title').textContent = dp.title;
+        if(dp.when == 'a-bigpage') {
+            document.getElementById('a-epis').click();            
+        }
         
+        if (dp.when == 'a-enviar' && radios.every(r => r.checked)) {
+            console.log('Si')
+            document.getElementById('enviar').textContent = 'Si'
+        } 
+        else {
+            console.log('No')
+            document.getElementById('enviar').textContent = 'No'
+        }
+        
+        document.getElementById('title').textContent = dp.title;        
     });
 }
-function addRadioListeners () {
-    let r = document.getElementsByTagName('input');
-    Object.values(r).filter((item) => item.type == "radio").forEach((item) => {
-        item.addEventListener('click',function(event) {
-            //event.preventDefault();                 
-            if(item.checked) {                
-                document.getElementById(item.name).innerHTML = "&#10004;";
-                //document.getElementById(item.name).style.color = "green"
-                document.getElementById(item.name).classList.remove('unchecked');
-                document.getElementById(item.name).classList.add('checked');
 
-            } 
-            else {
-                document.getElementById(item.name).innerHTML="&#10008";                 
-                //document.getElementById(item.name).style.color = "red"
-                document.getElementById(item.name).classList.remove('checked');
-                document.getElementById(item.name).classList.add('unchecked');
-
-            }       
-        });            
-    });
+function addRadioListener (r) {
+    r.addEventListener('click',function(event) {        
+        r.checked = true;
+        if(r.checked) {                
+            document.getElementById(r.name).innerHTML = "&#10004;";
+            document.getElementById(r.name).classList.remove('unchecked');
+            document.getElementById(r.name).classList.add('checked');
+        } 
+        /*
+        else {
+            document.getElementById(r.name).innerHTML="&#10008";                 
+            document.getElementById(r.name).classList.remove('checked');
+            document.getElementById(r.name).classList.add('unchecked');
+        } 
+        */      
+    });            
 }
